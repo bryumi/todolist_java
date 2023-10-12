a74fed9b10c2d78048068bd176221ec8507e50ec
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2023-10-11 21:14:54 -0300

    aula 02-integração com banco

--- src/main/java/br/com/brunayumi/todolist/user/UserModel.java
+++ src/main/java/br/com/brunayumi/todolist/user/UserModel.java
@@ -1,7 +1,34 @@
 package br.com.brunayumi.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
 public class UserModel {
-    public String username;
-    public String name;
-    public String password;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    // nomes das colunas do banco de dados são exatamente esses
+    // se nao forem é preciso usar @column
+
+    @Column (unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    //getters e setters atraves do lombook
+
 }
