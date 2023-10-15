a7015b101791852061bdacaa2c373bda431eebff
Bruna Yumi Nagahashi <bruna.nagahashi@gmail.com>
2023-10-15 18:03:37 -0300

    aula 4- validações e update de tasks

--- src/main/java/br/com/brunayumi/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/brunayumi/todolist/filter/FilterTaskAuth.java
@@ -26,7 +26,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
                 var servletPath = request.getServletPath();
 
-                if (servletPath.equals("/tasks/")){
+                if (servletPath.startsWith("/tasks/")){
                     //Pegar a autenticação (usuario e senha)
                     var authorization = request.getHeader("Authorization");
                     var authEncoded = authorization.substring("Basic".length()).trim();
@@ -45,7 +45,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                         //validar senha
                         var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
                         if (passwordVerify.verified) {
-
+                            request.setAttribute("idUser", user.getId());
                             filterChain.doFilter(request, response);
                         } else {
                             response.sendError(401);
